d086f6a35de20c05d6233b6e5c04be5964da88b5
Guy Montal <guymontal@gmail.com>
2022-03-15 15:14:57 +0200

    Changes for deployment on heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = searchElement.value;
     locationElement.textContent = 'Loading data...';
     forcastElement.textContent = '';
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 locationElement.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
 const path = require('path')
 const express = require('express');
-const hbs = require('hbs')
-const geoCode = require('./utils/geocode')
-const weatherReport = require('./utils/forcast')
+const hbs = require('hbs');
+const geoCode = require('./utils/geocode');
+const weatherReport = require('./utils/forcast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 // Define paths for express config
-const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialPath = path.join(__dirname, '../templates/partials')
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 
 // Setup handlebars engine and views location.
-app.set('views', viewsPath)
-app.set('view engine', 'hbs')
-hbs.registerPartials(partialPath)
+app.set('views', viewsPath);
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialPath);
 
 //setup static directory to serve.
-app.use(express.static(publicDirPath))
+app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -44,7 +46,7 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    const userAddress = req.query.address
+    const userAddress = req.query.address;
     if (!userAddress) {
         return res.send({
             error: 'Please write an address to get your forcast.'
@@ -87,6 +89,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('The server is up and running...')
 })
